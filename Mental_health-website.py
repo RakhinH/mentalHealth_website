fa1e0aa8892b8b051d84da2623a66d9091b66755
RakhinH <rakhinchowdhury@icloud.com>
2024-06-19 12:59:33 +0100

    h

--- Mental_health-website.py
+++ Mental_health-website.py
@@ -1,6 +1,15 @@
+from tkinter import *
 import tkinter as tk 
 import datetime as dt 
 
-main_window = tk()# The main window where the options are represented 
+main_window = tk.Tk()# the main window where the options are represented 
+main_window.geometry("600x300")
+main_window.title("Main Menu")
+
+label1 = Label(main_window,text="Hello this is the main menu.")
+
+mainWindow_icon = PhotoImage(file='Screenshot 2024-01-10 130335.png')
+main_window.iconphoto(True,mainWindow_icon)
+main_window.config(background="#EDCDFF")
 
 main_window.mainloop()
--- Mental_health-website;python.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk 
-import datetime as dt 
-
-main_window = tk()# The main window where the options are represented 
-
-main_window.mainloop()
