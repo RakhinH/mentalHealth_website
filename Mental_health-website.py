a6f94f974a66a60f229329a120f72c98aa32a050
RakhinH <rakhinchowdhury@icloud.com>
2024-07-02 14:10:48 +0100

    fix tmo morning 02/0724

--- Mental_health-website.py
+++ Mental_health-website.py
@@ -9,24 +9,41 @@ main_window.title("Main Menu")
 
 #---------------------------------------------------------------------------------------------------------------
 
-def hide_widget():
+def Ahide_widget():
     anger.grid_forget()
-    b1.configure(text="Show", command=show_widget)
-def show_widget():
+    Ab1.configure(text="Show", command=Ashow_widget)
+def Ashow_widget():
     anger.grid(row=1,column=3)
-    b1.configure(text="Hide", command=hide_widget)
+    Ab1.configure(text="Hide", command=Ahide_widget)
 
-#Below is 
+def Dhide_widget():
+    depression.grid_forget()
+    Db1.configure(text="Show", command=Dshow_widget)
+def Dshow_widget():
+    depression.grid(row=2,column=3)
+    Db1.configure(text="Hide", command=Dhide_widget)
+
+#---------------------------------------------------------------------------------------------------------------
+
+#Below is anger
 angerCho = tk.Label(main_window, text="Anger \nManagement", font='Aerial')
 angerCho.grid(row=1,column=1)
 
-#Below is the labels used for this indivdual window
 anger = tk.Label(main_window, text="To manage anger effectively, start by pausing and taking deep breaths to calm \nyourself before reacting, which helps prevent impulsive actions. \nReflect on the specific triggers that caused your anger to understand the root of \nthe issue and develop better strategies for handling similar situations in the future.\nWhen expressing your feelings, use 'I' statements like 'I feel upset when...' \nto communicate your emotions constructively without blaming others, promoting a more \npeaceful and productive dialogue.", font=('Aerial 8'))
 
-# Add a Button widget
-b1 = tk.Button(main_window, text="Show", command=show_widget)
-b1.grid(row=1,column=2,padx=1,pady=40)
+Ab1 = tk.Button(main_window, text="Show", command=Dshow_widget)
+Ab1.grid(row=1,column=2,padx=1,pady=40)
 
+#Below is depression
+depressionCho = tk.Label(main_window, text="Anger \nManagement", font='Aerial')
+depressionCho.grid(row=2,column=1)
+
+
+depression = tk.Label(main_window, text="To manage anger effectively, start by pausing and taking deep breaths to calm \nyourself before reacting, which helps prevent impulsive actions. \nReflect on the specific triggers that caused your anger to understand the root of \nthe issue and develop better strategies for handling similar situations in the future.\nWhen expressing your feelings, use 'I' statements like 'I feel upset when...' \nto communicate your emotions constructively without blaming others, promoting a more \npeaceful and productive dialogue.", font=('Aerial 8'))
+
+# Add a Button widget
+Db1 = tk.Button(main_window, text="Show", command=Dshow_widget)
+Db1.grid(row=2,column=2,padx=1,pady=40)
 
 #---------------------------------------------------------------------------------------------------------------
 
